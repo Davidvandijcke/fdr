ca5c625d3dc3de0d66b5f56f9af33ea738d24945
Davidvandijcke <david.van.dijcke@gmail.com>
2023-04-03 16:32:03 -0400

    fixed a bunch of issues with the jumpfrom and jumpto arrays

--- src/FDD/FDD.py
+++ src/FDD/FDD.py
@@ -11,7 +11,8 @@ from sklearn.cluster import KMeans
 
 class FDD():
     def __init__(self, Y : np.array, X : np.array, level : int=16, 
-                 lmbda : float=1, nu : float=0.01, iter : int=1000, tol : float=5e-5, rectangle : bool=False) -> None:
+                 lmbda : float=1, nu : float=0.01, iter : int=1000, tol : float=5e-5, rectangle : bool=False, 
+                 qtile : float=0.05) -> None:
 
         self.device = self.setDevice()
         torch.set_grad_enabled(False)
@@ -27,6 +28,7 @@ class FDD():
         self.iter = iter
         self.tol = tol
         self.rectangle = rectangle
+        self.qtile = qtile
         
         self.normalizeData() # scale data to unit hypercube
         self.castDataToGrid()
@@ -86,10 +88,11 @@ class FDD():
             distances = np.sqrt(np.sum((self.X[:,None,:] - self.X[None,:,:])**2, axis = 2))
         np.fill_diagonal(distances, 1) # remove self-comparisons
         distances = np.min(distances, axis = 0) # get closest point for each point
-        qile = np.quantile(distances, 0.005) # get 0.5% quantile
+        qile = np.quantile(distances, self.qtile) # get 5% quantile
         
         # pythagoras
-        self.resolution = qile # take as side-length the 0.5% quantile of distances between points
+        
+        self.resolution = 2*  qile / np.sqrt(2) # on average, points fall in center of grid cell, then use Pythagoras to get resolution
         
         xmax = np.max(self.X, axis = 0)
         
@@ -113,7 +116,7 @@ class FDD():
             grid_y[it.multi_index] = self.Y[closest_seed] #.min()
 
             # assign original x value
-            grid_x_og[it.multi_index] = tuple(self.X[closest_seed,:])
+            grid_x_og[it.multi_index] = tuple(self.X_raw[closest_seed,:])
         
         if self.Y.ndim == 1:
             grid_y = grid_y.reshape(grid_y.shape + (1,))
@@ -142,7 +145,7 @@ class FDD():
         
         return h_img
     
-    def k_means_boundary(self, u):
+    def k_means_boundary(self):
         # histogram of gradient norm
         test = np.linalg.norm(forward_differences(mIn, D = len(mIn.shape)), axis = 0)
         out = plt.hist(test)
@@ -155,10 +158,10 @@ class FDD():
         kmeans = KMeans(n_clusters=2, random_state=0).fit(Z)
         nu = X1[kmeans.labels_ == 1].max()
         
-    def boundaryGridToData(self, J_grid):
+    def boundaryGridToData(self, J_grid, u):
         # get the indices of the J_grid where J_grid is 1
         k = np.array(np.where(J_grid == 1))
-        
+                
         distances = [] # TODO: can't jump to another boundary point
         k_shifts = []
         for d in range(k.shape[0]):
@@ -192,25 +195,32 @@ class FDD():
         k = k[:,idx]
         idx = np.argmin(distances, axis = 0)
         
-        closest_points = k_shifts[idx, :, np.arange(k_shifts.shape[2])] # these are the coordinates of the boundary points
-        midpoints = (k.T + closest_points) / 2
+        closest_points = k_shifts[idx, :, np.arange(k_shifts.shape[2])] # these are the coordinates of the jump to points
+        midpoints = (k.T + closest_points) / 2 # these are the estimated boundary points
+        
+        #closest_points = ((closest_points / self.grid_x.shape[:-1])) 
 
         # get jumpto points
-        matching_rows = np.all(self.X_raw[:, np.newaxis] == closest_points, axis=-1)
-        matching_indices = np.where(matching_rows)[0]
-        Y_jumpto = self.Y_raw[matching_indices]
+        # matching_rows = np.all(self.X_raw[:, np.newaxis] == closest_points, axis=-1)
+        # matching_indices = np.where(matching_rows)[0]
+        
+        # first scale u back
+        u_scaled = u *  np.max(self.Y_raw, axis = 0)
+        
+        # get the points of self.grid_x_og with the index (x,y) for each row [x,y] in closest_points
+        closest_x = np.array([self.grid_x_og[tuple(i)] for i in closest_points])
+        Y_jumpto =  np.array([u_scaled[tuple(i)] for i in closest_points])
         
         # get jumpfrom points
-        matching_rows = np.all(self.X_raw[:, np.newaxis] == np.transpose(k), axis=-1)
-        matching_indices = np.where(matching_rows)[0]
-        Y_jumpfrom = self.Y_raw[matching_indices]
+        closest_x_shift = np.array([self.grid_x_og[tuple(i)] for i in k.T])
+        Y_jumpfrom =  np.array([u_scaled[tuple(i)] for i in k.T])
         
         # jump size
         jumpsize = Y_jumpto - Y_jumpfrom
         
         # create named array to return
         rays = [midpoints[:,d] for d in range(midpoints.shape[1])] + [Y_jumpfrom, Y_jumpto, jumpsize]
-        names = ["X_" + str(d) for d in range(midpoints.shape[1])] + ["Y_jumpto", "Y_jumpfrom", "Y_jumpsize"]
+        names = ["X_" + str(d) for d in range(midpoints.shape[1])] + ["Y_jumpfrom", "Y_jumpto", "Y_jumpsize"]
         jumps = np.core.records.fromarrays(rays, names=names)
         
         return jumps
@@ -238,7 +248,7 @@ class FDD():
         J_grid = (u_norm >= nu).astype(int)
         
         ## find the boundary on the point cloud
-        jumps = self.boundaryGridToData(J_grid)
+        jumps = self.boundaryGridToData(J_grid, u)
         
         # test_grid = np.zeros(self.grid_y.shape)
         # for row in jumps:
@@ -316,99 +326,142 @@ if __name__ == "__main__":
     dev = setDevice()
     g = DeviceMode(torch.device(dev))
     g.__enter__()
+    
+    
+    # #------ test 1
 
-    image = "resources/images/marylin.png"
-    mIn = cv2.imread(image, (0))
-    mIn = mIn.astype(np.float32)
-    mIn /= 255
+    # image = "resources/images/marylin.png"
+    # mIn = cv2.imread(image, (0))
+    # mIn = mIn.astype(np.float32)
+    # mIn /= 255
     
-    Y = mIn.flatten()
-    #Y = np.stack([Y, Y], axis = 1)
-    # get labels of grid points associated with Y values in mIn
-    X = np.stack([np.tile(np.arange(0, mIn.shape[0], 1), mIn.shape[1]), np.repeat(np.arange(0, mIn.shape[0], 1), mIn.shape[1])], axis = 1)
+    # Y = mIn.flatten()
+    # #Y = np.stack([Y, Y], axis = 1)
+    # # get labels of grid points associated with Y values in mIn
+    # X = np.stack([np.tile(np.arange(0, mIn.shape[0], 1), mIn.shape[1]), np.repeat(np.arange(0, mIn.shape[0], 1), mIn.shape[1])], axis = 1)
     
-    # reshuffle Y and X in the same way so that it resembles normal data
-    idx = np.random.permutation(Y.shape[0])
-    Y = Y[idx]
-    X = X[idx]
+    # # reshuffle Y and X in the same way so that it resembles normal data
+    # idx = np.random.permutation(Y.shape[0])
+    # Y = Y[idx]
+    # X = X[idx]
             
-    def forward_differences(ubar, D : int):
+    # def forward_differences(ubar, D : int):
 
-        diffs = []
+    #     diffs = []
 
-        for dim in range(int(D)):
-            #zeros_shape = torch.jit.annotate(List[int], [])
-            zeros_shape = list(ubar.shape)
-            zeros_shape[dim] = 1
-            # for j in range(ubar.dim()): 
-            #     if j == dim:
-            #         zeros_shape.append(1)
-            #     else:
-            #         zeros_shape.append(ubar.shape[j])
-            zeros = np.zeros(zeros_shape)
-            diff = np.concatenate((np.diff(ubar, axis=dim), zeros), axis=dim)
-            diffs.append(diff)
-                    # Stack the results along a new dimension (first dimension)
-        u_star = np.stack(diffs, axis=0)
+    #     for dim in range(int(D)):
+    #         #zeros_shape = torch.jit.annotate(List[int], [])
+    #         zeros_shape = list(ubar.shape)
+    #         zeros_shape[dim] = 1
+    #         # for j in range(ubar.dim()): 
+    #         #     if j == dim:
+    #         #         zeros_shape.append(1)
+    #         #     else:
+    #         #         zeros_shape.append(ubar.shape[j])
+    #         zeros = np.zeros(zeros_shape)
+    #         diff = np.concatenate((np.diff(ubar, axis=dim), zeros), axis=dim)
+    #         diffs.append(diff)
+    #                 # Stack the results along a new dimension (first dimension)
+    #     u_star = np.stack(diffs, axis=0)
 
-        return u_star
+    #     return u_star
     
-    def boundary(u, nu):
-        u_diff = forward_differences(u, D = len(u.shape))
-        u_norm = np.linalg.norm(u_diff, axis = 0, ord = 2) # 2-norm
-        return (u_norm >= np.sqrt(nu)).astype(int)
+    # def boundary(u, nu):
+    #     u_diff = forward_differences(u, D = len(u.shape))
+    #     u_norm = np.linalg.norm(u_diff, axis = 0, ord = 2) # 2-norm
+    #     return (u_norm >= np.sqrt(nu)).astype(int)
     
 
-    # histogram of gradient norm
-    test = np.linalg.norm(forward_differences(mIn, D = len(mIn.shape)), axis = 0)
-    out = plt.hist(test)
+    # # histogram of gradient norm
+    # test = np.linalg.norm(forward_differences(mIn, D = len(mIn.shape)), axis = 0)
+    # out = plt.hist(test)
 
-    X1 = np.tile(out[1][1:], out[0].shape[0])
-    X2 = out[0].reshape(-1,1).squeeze(1)
-    Z = np.stack([X1, X2], axis = 1)
+    # X1 = np.tile(out[1][1:], out[0].shape[0])
+    # X2 = out[0].reshape(-1,1).squeeze(1)
+    # Z = np.stack([X1, X2], axis = 1)
 
-    from sklearn.cluster import KMeans
+    # from sklearn.cluster import KMeans
 
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(Z)
-    nu = X1[kmeans.labels_ == 1].max()
+    # kmeans = KMeans(n_clusters=2, random_state=0).fit(Z)
+    # nu = X1[kmeans.labels_ == 1].max()
 
-    model = FDD(Y, X, level = 16, lmbda = 1, nu = 0.05, iter = 1000, tol = 5e-5)
-    u, jumps, J_grid, nrj, eps, it = model.run()
-    cv2.imwrite("result.png",u*255)
+    # model = FDD(Y, X, level = 16, lmbda = 1, nu = 0.05, iter = 1000, tol = 5e-5)
+    # u, jumps, J_grid, nrj, eps, it = model.run()
+    # cv2.imwrite("result.png",u*255)
 
-    plt.imshow(J)
+    # plt.imshow(J)
 
-    # histogram of gradient norm
+    # # histogram of gradient norm
 
-    test = np.linalg.norm(forward_differences(u, D = len(u.shape)), axis = 0)
-    out = plt.hist(test)
+    # test = np.linalg.norm(forward_differences(u, D = len(u.shape)), axis = 0)
+    # out = plt.hist(test)
 
-    X = np.tile(out[1][1:], out[0].shape[0])
-    Y = out[0].reshape(-1,1).squeeze(1)
-    Z = np.stack([X, Y], axis = 1)
+    # X = np.tile(out[1][1:], out[0].shape[0])
+    # Y = out[0].reshape(-1,1).squeeze(1)
+    # Z = np.stack([X, Y], axis = 1)
 
-    from sklearn.cluster import KMeans
+    # from sklearn.cluster import KMeans
 
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(Z)
-    nu = X[kmeans.labels_ == 1].max()
+    # kmeans = KMeans(n_clusters=2, random_state=0).fit(Z)
+    # nu = X[kmeans.labels_ == 1].max()
 
-    # get new boundary
-    J_new = boundary(u, nu = nu**2) # the squared is just cause we're taking the square root
+    # # get new boundary
+    # J_new = boundary(u, nu = nu**2) # the squared is just cause we're taking the square root
 
-    plt.imshow(J_new)
+    # plt.imshow(J_new)
 
-    # plt.hist(test.reshape(-1,1)[kmeans.labels_ == 2], bins = 100)
+    # # plt.hist(test.reshape(-1,1)[kmeans.labels_ == 2], bins = 100)
 
         
-    # from pomegranate import  *
+    # # from pomegranate import  *
 
-    # model = GeneralMixtureModel.from_samples(NormalDistribution, n_components=2, X=test.reshape(-1,1))
-    # labels = model.predict(test.reshape(-1,1))
+    # # model = GeneralMixtureModel.from_samples(NormalDistribution, n_components=2, X=test.reshape(-1,1))
+    # # labels = model.predict(test.reshape(-1,1))
 
-    # plt.hist(test.reshape(-1,1)[labels == 0], bins = 100)
+    # # plt.hist(test.reshape(-1,1)[labels == 0], bins = 100)
 
-    # plt.hist(test.reshape(-1,1),  bins = 500)
-    # plt.ylim(0,5)
+    # # plt.hist(test.reshape(-1,1),  bins = 500)
+    # # plt.ylim(0,5)
+    
+    
+    
+    #------- test2
+    
+    # Generate some random data points from a discontinuous function
+    np.random.seed(0)
+    data = np.random.rand(1000, 2) # draw 1000 2D points from a uniform
+
+    # Create the grid
+    # Define the grid dimensions and resolution
+    xmin, xmax = 0, 1
+    ymin, ymax = 0, 1
+    resolution = 0.01 # 100 by 100 grid
+    x, y = np.meshgrid(np.arange(xmin, xmax, resolution), np.arange(ymin, ymax, resolution))
+    grid = np.dstack((x, y))
+    grid_f = np.zeros(grid.shape[:2])
+
+    def f(x,y):
+        temp = np.sqrt((x-1/2)**2 + (y-1/2)**2)
+        if temp < 1/4:
+            return temp
+        else:
+            return temp + 1/8
+
+    # Compute the function values on the grid
+    for i in range(grid.shape[0]):
+        for j in range(grid.shape[1]):
+            grid_f[i, j] = f(grid[i, j][0], grid[i, j][1])
+            
+    # now sample the function values on the data points
+    grid_sample = np.zeros((data.shape[0],1))
+    for i in range(data.shape[0]):
+            grid_sample[i] = f(data[i,0], data[i,1])
+
+    X = data.copy()
+    Y = grid_sample.copy().flatten()
+    # and run the FDD command
+    model = FDD(Y, X, level = 16, lmbda = 1, nu = 0.05, iter = 1000, tol = 5e-5, qtile = 0.1)
+    u, jumps, J_grid, nrj, eps, it = model.run()
 
 
 
