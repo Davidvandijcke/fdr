1b77fc8fc07f6c79309e6b92cdf7d7cc5abff8b5
Davidvandijcke <david.van.dijcke@gmail.com>
2023-11-07 22:54:54 -0500

    set up subsampling for india econ

---
+++ src/analysis/subsampling_india_econ.py
@@ -0,0 +1,81 @@
+import pandas as pd 
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from rasterio.mask import raster_geometry_mask, mask
+import cv2
+from FDD import FDD
+from FDD.SURE import SURE
+import geopandas as gpd
+import pickle
+import boto3
+
+
+def moveUp(fn, times = 1):
+    for _ in range(times):
+        fn = os.path.dirname(fn)
+    return fn
+
+if __name__ == '__main__': 
+    
+     #----------------
+    # parameters
+    #----------------
+    S = 32
+    N = 1000
+    num_gpus = 1
+    num_cpus = 2
+    
+    # get directory above
+    main_dir = "/home/dvdijcke/" # moveUp(dir, 4)
+    data_in = os.path.join(main_dir, 'data', 'in')    
+    data_out = os.path.join(main_dir, 'data', 'out')  
+    
+    # get SURE parameters
+    fto = os.path.join(data_out, 'india_econ_SURE_90_lambda25_nu025.pkl') 
+    with open(fto, "rb") as f:
+        res = pickle.load(f)
+
+    best = res.get_best_result(metric = "score", mode = "min")
+
+    config = best.metrics['config']
+    lmbda, nu = config['lmbda'], config['nu']
+
+    fn_merged = os.path.join(data_out, 'india', 'rajasatan_cheating_shops_merged_40K.geojson')
+    gdf = gpd.read_file(fn_merged)
+    
+    gdf['pings_norm'] = gdf['pings_norm'] * 100
+    print(gdf.head())
+    Y = np.array(gdf['pings_norm'])
+    X = np.stack([np.array(gdf.geometry.centroid.x), np.array(gdf.geometry.centroid.y)]).T
+
+    qtile = np.quantile(Y, 0.90)
+    Y[Y>qtile] = qtile
+        
+    resolution = 1/int(np.sqrt(Y.size))
+    model = FDD(Y, X, level = 32, lmbda = lmbda, nu = nu, iter = 10000, tol = 5e-5, resolution=resolution,
+        pick_nu = "MS", scaled = True, scripted = False, rectangle=True)
+    results = model.run()
+    u = results['u']
+    fn = "india_u.npy"
+    np.save(fn, u)
+    
+    
+    s3 = boto3.client('s3')
+    with open(fn, "rb") as f:
+        s3.upload_fileobj(f, "projects-fdd", "data/out/subsampling/" + fn)
+        
+    (test, b) = model.subSampling(nboot = 500)    
+    test = np.stack(test, axis=0)
+    fn = "india_boots.npy"
+    np.save(fn, test)
+    with open(fn, "rb") as f:
+        s3.upload_fileobj(f, "projects-fdd", "data/out/subsampling/" + fn)
+        
+    fn = "india_b.npy"
+    np.save(fn, b)
+    with open(fn, "rb") as f:
+        s3.upload_fileobj(f, "projects-fdd", "data/out/subsampling/" + fn)
+    
+        
+
